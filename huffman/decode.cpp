599a5935c48fb7ad7bda16cc324d7d5830d49bf5
vks4git <vks_m@mail.ru>
2015-11-24 01:05:12 +0300

    Huffman code

--- huffman/decode.cpp
+++ huffman/decode.cpp
@@ -1,8 +1,28 @@
-#include <iostream.h>
+#include <bits/stdc++.h>
 
 using namespace std;
 
 int main() {
-
-return 0;
+    int k;
+    int l;
+    cin >> k >> l;
+    unordered_map<string, char> dict;
+    for (int i = 0; i < k; ++i) {
+        string sym;
+        string code;
+        cin >> sym >> code;
+        dict[code] = sym[0];
+    }
+    string encoded;
+    cin >> encoded;
+    string part = "";
+    for (int i = 0; i < l; ++i) {
+        part += encoded[i];
+        if (dict[part] != 0) {
+            cout << dict[part];
+            part = "";
+        }
+    }
+    cout << endl;
+    return 0;
 }
--- huffman/encode.cpp
+++ huffman/encode.cpp
@@ -1,8 +1,79 @@
-#include <iostream.h>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-int main() {
+class Node {
+public:
+    Node(int frequency, char symbol) : frequency(frequency), symbol(symbol), left(NULL), right(NULL) { }
 
-return 0;
+    Node *left;
+    Node *right;
+    int frequency;
+    char symbol;
+};
+
+struct greater_node : public binary_function<Node *, Node *, bool> {
+    bool
+    operator()(const Node *__x, const Node *__y) const { return __x->frequency > __y->frequency; }
+};
+
+void dfs(Node *root, string code, unordered_map<char, string> &map) {
+    if (root == NULL) {
+        return;
+    }
+    dfs(root->left, code + "0", map);
+    if (root->symbol != 0) {
+        map[root->symbol] = code;
+    }
+    dfs(root->right, code + "1", map);
+}
+
+int main() {
+    priority_queue<Node *, vector<Node *>, greater_node> pq;
+    string s;
+    cin >> s;
+    vector<int> frequency(26, 0);
+    for (char c : s) {
+        frequency[c - 'a']++;
+    }
+    for (int i = 0; i < 26; ++i) {
+        if (frequency[i] == 0) {
+            continue;
+        }
+        Node * sym = new Node(frequency[i], (char) (i + 'a'));
+        pq.push(sym);
+    }
+    while (pq.size() != 1) {
+        Node *f1 = pq.top();
+        pq.pop();
+        Node *f2 = pq.top();
+        pq.pop();
+        Node * parent = new Node(f1->frequency + f2->frequency, 0);
+        parent->left = f1;
+        parent->right = f2;
+        pq.push(parent);
+    }
+    Node *root = pq.top();
+    string code = "";
+    if (root->symbol != 0) {
+        code = "0";
+    }
+    unordered_map<char, string> dict;
+    dfs(root, code, dict);
+    int len = 0;
+    for (char c : s) {
+        len += dict[c].length();
+    }
+    cout << dict.size() << ' ' << len << endl;
+    for (int i = 0; i < 26; ++i) {
+        if (frequency[i] == 0) {
+            continue;
+        }
+        cout << (char) (i + 'a') << ": " << dict[i + 'a'] << endl;
+    }
+    for (char c : s) {
+        cout << dict[c];
+    }
+    cout << endl;
+    return 0;
 }
